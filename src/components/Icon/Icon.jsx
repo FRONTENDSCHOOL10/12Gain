886393721b83201789d711f4a03f554980bbcccb
seungsu-K <kss095@naver.com>
2024-08-31 16:56:32 +0900

    [feat] 아이콘 컴포넌트 수정
    - text prop 추가
    - text 스타일 추가

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,24 @@
+import S from './style.module.css';
+
+import { number } from 'prop-types';
+import { string } from 'prop-types';
+
+Icon.propTypes = {
+  id: string.isRequired,
+  width: number,
+  height: number,
+  text: string,
+};
+
+function Icon({ id, width = 20, height = 20, text = null }) {
+  return (
+    <div className={S.component}>
+      <svg width={width} height={height}>
+        <use xlinkHref={`/stack.svg#${id}`} />
+      </svg>
+      <span>{text}</span>
+    </div>
+  );
+}
+
+export default Icon;
