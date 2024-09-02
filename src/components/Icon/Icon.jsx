a94674da3f01586fda3ec6844f586608e3569da8
seungsu-K <kss095@naver.com>
2024-09-03 00:48:40 +0900

    [docs]
    - 기본 프로필 이미지 추가
    - svg 아이콘 추가
    - 아이콘 컴포넌트 색상 prop 추가
    - button, a 리셋 설정

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -8,12 +8,13 @@ Icon.propTypes = {
   width: number,
   height: number,
   text: string,
+  color: string,
 };
 
-function Icon({ id, width = 20, height = 20, text = null }) {
+function Icon({ id, width = 20, height = 20, text = null, color = '#101115' }) {
   return (
     <div className={S.component}>
-      <svg width={width} height={height}>
+      <svg width={width} height={height} style={{ color }}>
         <use xlinkHref={`/stack.svg#${id}`} />
       </svg>
       <span>{text}</span>
