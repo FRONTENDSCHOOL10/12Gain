f4fe97bf3d8e41c1d9a7d2e092a409ec5de714fc
seungsu-K <kss095@naver.com>
2024-09-03 00:49:01 +0900

    [docs] App 리팩토리

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,16 +8,6 @@ function App() {
   const record = pb.collection('test');
   console.log(record);
 
-  // const [inputValue, setInputValue] = useState('');
-
-  // const handleChange = (e) => {
-  //   setInputValue(e.target.value);
-  // };
-
-  // const handleClear = () => {
-  //   setInputValue('');
-  // };
-
   return (
     <StrictMode>
       <RouterProvider router={router} />
