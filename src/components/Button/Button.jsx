a5804b9ec8b38d47fb09e3dcbdb8f58914a5a226
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-09-08 17:22:49 +0900

    [feat] 버튼 컴포넌트 수정
    - text 삭제
    - style 삭제 후 className으로 받게 함
    - type 설정 가능

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,38 +1,27 @@
-import { string, bool, func, object } from 'prop-types';
+import { string, bool, func, node } from 'prop-types';
 import S from './Button.module.css';
 
-function Button({
-  text,
-  width = '18.5rem',
-  height = '2.25rem',
-  color = 'var(--white)',
-  backgroundColor = 'var(--primary)',
-  disabled = false,
-  onClick,
-  children,
-}) {
+function Button({ type, className, disabled = false, onClick, children }) {
   return (
-    <button
-      className={S.Button}
-      style={{ width, height, color, backgroundColor }}
-      disabled={disabled}
-      onClick={onClick}
-    >
-      {text}
-      {children}
-    </button>
+    <div className={S.Button}>
+      <button
+        type={type}
+        className={className}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
   );
 }
 
 Button.propTypes = {
-  text: string,
-  width: string,
-  height: string,
-  color: string,
-  backgroundColor: string,
+  type: string,
+  className: string,
   disabled: bool,
   onClick: func,
-  children: object,
+  children: node,
 };
 
 export default Button;
