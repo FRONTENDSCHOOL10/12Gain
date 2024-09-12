2c4beaa767bf62d44cf23177d833e4c0de29d8dc
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-09-12 15:06:11 +0900

    [feat] 케밥메뉴 구현 중

--- src/components/KebabMenu/KebabMenu.jsx
+++ src/components/KebabMenu/KebabMenu.jsx
@@ -1,16 +1,70 @@
-import Button from '@/components/Button/Button';
-import Icon from '@/components/Icon/Icon';
 import S from '@/components/KebabMenu/style.module.css';
+import IconButton from '@/components/Button/IconButton';
+import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import pb from '@/api/pb';
+import Button from '../Button/Button';
+import { useCallback } from 'react';
 
-function KebabMenu() {
-  const onClickMenu = () => {
-    console.log('메뉴창 열림');
-  };
+function KebabMenu({ postAuthorId }) {
+  const [isOptionOpen, setIsOptionOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await pb.authStore.model;
+      setCurrentUser(user);
+    };
+
+    fetchUser();
+  }, []);
+
+  const handleOpenMenu = useCallback(() => {
+    setIsOptionOpen((open) => !open);
+    console.log('옵션창 열렸습니당?');
+  }, []);
+
+  const handleCloseMenu = useCallback(() => {
+    setIsOptionOpen((close) => !close);
+    console.log('옵션창 닫혔습니당?');
+  }, []);
+
+  const isAuthor = currentUser && currentUser.id === postAuthorId;
 
   return (
-    <Button className={S.component} onClick={onClickMenu}>
-      <Icon id="more" width={20} height={20} />
-    </Button>
+    <div className="button-icon">
+      <IconButton
+        title="옵션 선택"
+        iconId="more"
+        width={20}
+        height={20}
+        onClick={handleOpenMenu}
+      />
+      {isOptionOpen && (
+        <div className={S.option}>
+          <div className={S.option__content}>
+            {isAuthor ? (
+              <div>
+                <NavLink to="my edit post or feed" className={S.option__button}>
+                  수정하기
+                </NavLink>
+                <Button className={S.option__button}>삭제하기</Button>
+                <Button className={S.option__button} onClick={handleCloseMenu}>
+                  취소
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <Button className={S.option__button}>신고하기</Button>
+                <Button className={S.option__button} onClick={handleCloseMenu}>
+                  취소
+                </Button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
