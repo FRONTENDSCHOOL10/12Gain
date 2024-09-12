389a11bfa376b699d4b135ff455a1ab9aa64787c
soyeonpaark <thdus906@naver.com>
2024-09-12 09:58:10 +0900

    [feat] 이메일 인풋 툴팁 추가

--- src/routes/login/component/EmailInput.jsx
+++ src/routes/login/component/EmailInput.jsx
@@ -1,5 +1,6 @@
 import InputWithDelete from '@/components/Input/InputWithDelete';
 import S from '@/components/Input/style.module.css';
+import Tooltip from '@/components/Tooltip/Tooltip';
 import { useLoginForm } from '@/stores/route';
 
 function EmailInput() {
@@ -7,16 +8,18 @@ function EmailInput() {
   const isEmailValid = useLoginForm((state) => state.isEmailValid);
   return (
     <div>
-      <InputWithDelete
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className={`paragraph-md ${email && !isEmailValid ? S.error : ''}`}
-        placeholder="이메일"
-        error={email !== '' && !isEmailValid}
-        hasInput={email !== '' && isEmailValid}
-        ariaLabel="이메일 주소 입력"
-      />
+      <Tooltip text="이메일 입력" position="bottom">
+        <InputWithDelete
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className={`paragraph-md ${email && !isEmailValid ? S.error : ''}`}
+          placeholder="이메일"
+          error={email !== '' && !isEmailValid}
+          hasInput={email !== '' && isEmailValid}
+          ariaLabel="이메일 입력"
+        />
+      </Tooltip>
     </div>
   );
 }
