2a630a75378a525b6314e560c63ead3f466f2577
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-09-23 01:00:08 +0900

    [feat] 프로필 페이지 - 내 피드 불러오기

--- src/routes/profile/component/MyFeed.jsx
+++ src/routes/profile/component/MyFeed.jsx
@@ -1,27 +1,63 @@
+import { useState, useEffect } from 'react';
 import S from '@/routes/profile/component/MyFeed.module.css';
-
 import FeedCard from '@/routes/profile/component/FeedCard';
+import pb from '@/api/pb';
+import LoadingSpinner from '@/components/LoadingSpinner/LoadingSpinner';
+import getPbImageURL from '@/api/getPbImageURL';
 
 export function Component() {
+  const [userFeeds, setUserFeeds] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserFeeds = async () => {
+      try {
+        setIsLoading(true);
+        const currentUser = JSON.parse(localStorage.getItem('pocketbase_auth'))
+          ?.model.id;
+
+        if (!currentUser) {
+          throw new Error('로그인된 사용자를 찾을 수 없습니다.');
+        }
+        const feeds = await pb.collection('feeds').getFullList();
+        const writer = feeds.filter((myFeed) => myFeed.writer === currentUser);
+        console.log(feeds);
+
+        setUserFeeds(
+          writer.map((item) => ({
+            id: item.id,
+            content: item.content,
+            writer: item.writer,
+            imageUrl:
+              item.image.length === 0
+                ? '/logo.webp'
+                : getPbImageURL(item, 'image'),
+            date: new Date(item.created).toLocaleDateString(),
+          }))
+        );
+      } catch (error) {
+        console.error('피드 불러오기 실패:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserFeeds();
+  }, []);
+
+  if (isLoading) return <LoadingSpinner />;
+  console.log(userFeeds);
+
   return (
     <div className={S.component}>
-      <FeedCard
-        content={
-          '상세 내용이 엄청나게 길어지면 ...으로 표현하기 위해서 테스트 용도로 넣은 텍스트인데 5줄까지 허용해서 엄청나게 길게 넣어야 댐. 가나다라마사바아차카타파하abcdefghifklmnopqrstuwxyz1234567890ㅂㄿㅋㅇㄻㄷㄻ'
-        }
-        url={'/profile.png'}
-        date={'2024.09.09'}
-      />
-      <FeedCard
-        content={'상세 내용'}
-        url={'/profile.png'}
-        date={'2024.09.09'}
-      />
-      <FeedCard
-        content={'상세 내용'}
-        url={'/profile.png'}
-        date={'2024.09.09'}
-      />
+      {userFeeds.map((feed) => (
+        <FeedCard
+          key={feed.id}
+          content={feed.content}
+          imageUrl={feed.imageUrl}
+          date={feed.date}
+        />
+      ))}
     </div>
   );
 }
