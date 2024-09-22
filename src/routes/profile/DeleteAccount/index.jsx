ada63b8f7dc1b43ad00fc8380dfcb7a0811705ef
hyoungsiyoung <hyoungsiyoung@gmail.com>
2024-09-22 16:56:30 +0900

    [refactory] 회원탈퇴 css 수정

--- src/routes/profile/DeleteAccount/index.jsx
+++ src/routes/profile/DeleteAccount/index.jsx
@@ -69,9 +69,11 @@ export function Component() {
           </label>
         </div>
       </div>
-      <Button className={S.button} onClick={handleDeleteAccount}>
-        탈퇴하기
-      </Button>
+      <div className={S.buttonBox}>
+        <Button className={S.button} onClick={handleDeleteAccount}>
+          탈퇴하기
+        </Button>
+      </div>
     </div>
   );
 }
